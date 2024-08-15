f9b827beb8c90bc355f00de0dfe536a433012d58
Jukelyn <viperkillergaming@gmail.com>
2024-08-15 10:59:28 -0400

    Refactoring

--- src/main.py
+++ src/main.py
@@ -48,83 +48,6 @@ def exit_program() -> None:
     sys.exit(0)
 
 
-def parse_ics(file_path: str, start: datetime, end: datetime) -> list[dict]:
-    """
-    Parse the calendar file to extract events, handling both single
-    occurrences and recurrence rules, and return them as a list
-    of event dictionaries.
-
-    Args:
-        file_path (str): The path to the file being parsed.
-        start (datetime): The starting date to parse from.
-        end (datetime): The ending date to end parsing.
-
-    Returns:
-        list[dict]: A list of the events that were parsed from the file.
-    """
-    try:
-        with open(file_path, 'rb') as f:
-            calendar = Calendar.from_ical(f.read())
-    except FileNotFoundError:
-        print(f"File {file_path} not found." +
-              " Check if file exists and also the spelling. " +
-              f"File structure should be:\n{FILE_STRUCT}")
-        exit_program()
-
-    events_list = []
-    single_occurrence_events = []
-
-    for component in calendar.walk():
-        if component.name == "VEVENT":
-            dtstart = component.get('dtstart').dt
-            dtend = component.get('dtend').dt
-            summary = str(component.get('summary'))
-
-            if dtstart.tzinfo:
-                dtstart = dtstart.astimezone(TIMEZONE)
-            else:
-                dtstart = TIMEZONE.localize(dtstart)
-            if dtend.tzinfo:
-                dtend = dtend.astimezone(TIMEZONE)
-            else:
-                dtend = TIMEZONE.localize(dtend)
-
-            duration = dtend - dtstart
-
-            # Handle single occurrence event
-            rule = component.get('rrule')
-            if not rule:
-                event = {  # pylint: disable=W0621
-                    'summary': summary,
-                    'dtstart': dtstart,
-                    'dtend': dtend,
-                    'duration': str(duration),
-                }
-                single_occurrence_events.append(event)
-
-    # Handle recurring events using the recurring_ical_events module
-    recurring_events = recurring_ical_events.of(calendar).between(start, end)
-    for event in recurring_events:
-        dtstart = event["dtstart"].dt
-        dtend = event["dtend"].dt if "dtend" in event else dtstart + duration
-
-        # Don't care about timezones, convert to offset-naive
-        if dtstart.tzinfo:
-            dtstart = dtstart.replace(tzinfo=None)
-        if dtend.tzinfo:
-            dtend = dtend.replace(tzinfo=None)
-
-        recurring_event = {
-            'summary': str(event.get('summary', 'Recurring Event')),
-            'dtstart': dtstart,
-            'dtend': dtend,
-            'duration': str(dtend - dtstart),
-        }
-        events_list.append(recurring_event)
-
-    return single_occurrence_events + events_list
-
-
 def get_cal_path(calendar_file: str) -> str:
     """
     Get's the calendar file and returns the path. Defaults to test calendar if
@@ -279,7 +202,121 @@ def get_end_day(default_date: datetime = start_day) -> datetime:
 
 
 end_day = get_end_day(start_day)
+
+
+def parse_ics(file_path: str, start: datetime, end: datetime) -> list[dict]:
+    """
+    Parse the calendar file to extract events, handling both single
+    occurrences and recurrence rules, and return them as a list
+    of event dictionaries.
+
+    Args:
+        file_path (str): The path to the file being parsed.
+        start (datetime): The starting date to parse from.
+        end (datetime): The ending date to end parsing.
+
+    Returns:
+        list[dict]: A list of the events that were parsed from the file.
+    """
+    try:
+        with open(file_path, 'rb') as f:
+            calendar = Calendar.from_ical(f.read())
+    except FileNotFoundError:
+        print(f"File {file_path} not found." +
+              " Check if file exists and also the spelling. " +
+              f"File structure should be:\n{FILE_STRUCT}")
+        exit_program()
+
+    events_list = []
+    single_occurrence_events = []
+
+    for component in calendar.walk():
+        if component.name == "VEVENT":
+            dtstart = component.get('dtstart').dt
+            dtend = component.get('dtend').dt
+            summary = str(component.get('summary'))
+
+            if dtstart.tzinfo:
+                dtstart = dtstart.astimezone(TIMEZONE)
+            else:
+                dtstart = TIMEZONE.localize(dtstart)
+            if dtend.tzinfo:
+                dtend = dtend.astimezone(TIMEZONE)
+            else:
+                dtend = TIMEZONE.localize(dtend)
+
+            duration = dtend - dtstart
+
+            # Handle single occurrence event
+            rule = component.get('rrule')
+            if not rule:
+                event = {  # pylint: disable=W0621
+                    'summary': summary,
+                    'dtstart': dtstart,
+                    'dtend': dtend,
+                    'duration': str(duration),
+                }
+                single_occurrence_events.append(event)
+
+    # Handle recurring events using the recurring_ical_events module
+    recurring_events = recurring_ical_events.of(calendar).between(start, end)
+    for event in recurring_events:
+        dtstart = event["dtstart"].dt
+        dtend = event["dtend"].dt if "dtend" in event else dtstart + duration
+
+        # Don't care about timezones, convert to offset-naive
+        if dtstart.tzinfo:
+            dtstart = dtstart.replace(tzinfo=None)
+        if dtend.tzinfo:
+            dtend = dtend.replace(tzinfo=None)
+
+        recurring_event = {
+            'summary': str(event.get('summary', 'Recurring Event')),
+            'dtstart': dtstart,
+            'dtend': dtend,
+            'duration': str(dtend - dtstart),
+        }
+        events_list.append(recurring_event)
+
+    return single_occurrence_events + events_list
+
+
 events = parse_ics(file_pathname, start_day, end_day)
+
+
+def remove_duplicate_events(events: list[dict]) -> list[dict]:
+    # pylint: disable=W0621
+    """
+    Removes duplicate events based on summary, start time, and end time.
+
+    Args:
+        events (list[dict]): A list of event dictionaries.
+
+    Returns:
+        list[dict]: The list of events with duplicates removed.
+    """
+    seen = set()
+    unique_events = []
+
+    for event in events:
+        # Ensure datetime objects are consistent
+        # Remove microseconds for consistency
+        dtstart = event['dtstart'].replace(tzinfo=None, microsecond=0)
+        dtend = event['dtend'].replace(tzinfo=None, microsecond=0)
+        summary = event.get('summary', '')
+
+        # Create a unique identifier for the event
+        event_id = (dtstart, dtend, summary)
+
+        # print(f"Checking event: {event_id}")  # Debugging output
+
+        if event_id not in seen:
+            seen.add(event_id)
+            unique_events.append(event)
+        # else:
+            # print(f"Duplicate found: {event_id}")  # Debugging output
+
+    return unique_events
 
 
 def get_events_between(start: datetime,
@@ -318,54 +355,6 @@ def get_events_between(start: datetime,
     unique_events = remove_duplicate_events(events_between)
 
     return (len(unique_events), unique_events)
-
-
-def format_datetime(dt: datetime) -> str:
-    """
-    Format a datetime object to a string in '%Y-%m-%d %H:%M:%S' format.
-
-    Args:
-        dt (datetime): A datetime object
-
-    Returns:
-        str: A formatted string of that datetime object.
-    """
-    return dt.strftime('%m-%d-%Y %H:%M:%S')
-
-
-def remove_duplicate_events(events: list[dict]) -> list[dict]:
-    # pylint: disable=W0621
-    """
-    Removes duplicate events based on summary, start time, and end time.
-
-    Args:
-        events (list[dict]): A list of event dictionaries.
-
-    Returns:
-        list[dict]: The list of events with duplicates removed.
-    """
-    seen = set()
-    unique_events = []
-
-    for event in events:
-        # Ensure datetime objects are consistent
-        # Remove microseconds for consistency
-        dtstart = event['dtstart'].replace(tzinfo=None, microsecond=0)
-        dtend = event['dtend'].replace(tzinfo=None, microsecond=0)
-        summary = event.get('summary', '')
-
-        # Create a unique identifier for the event
-        event_id = (dtstart, dtend, summary)
-
-        # print(f"Checking event: {event_id}")  # Debugging output
-
-        if event_id not in seen:
-            seen.add(event_id)
-            unique_events.append(event)
-        # else:
-            # print(f"Duplicate found: {event_id}")  # Debugging output
-
-    return unique_events
 
 
 def view_events_in_range(start: datetime,
@@ -451,6 +440,19 @@ def sort_events(events: list[dict]) -> list[dict]:  # pylint: disable=W0621
 
 
 events = sort_events(events)
+
+
+def format_datetime(dt: datetime) -> str:
+    """
+    Format a datetime object to a string in '%Y-%m-%d %H:%M:%S' format.
+
+    Args:
+        dt (datetime): A datetime object
+
+    Returns:
+        str: A formatted string of that datetime object.
+    """
+    return dt.strftime('%m-%d-%Y %H:%M:%S')
 
 
 def display_event_details(event: Optional[dict] = None) -> None:
